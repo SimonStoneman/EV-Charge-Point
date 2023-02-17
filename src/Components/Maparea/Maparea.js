d48e48bc043bab2d76812ed60ca6c8cf2fbf8c6a
SimonStoneman <116112151+SimonStoneman@users.noreply.github.com>
2023-02-17 19:43:53 +0000

    Adding comments to sample code in Maparea.js to describe how it works. Removed styling in Maparea.css as this was from a javascript sample

--- src/Components/Maparea/Maparea.js
+++ src/Components/Maparea/Maparea.js
@@ -13,21 +13,25 @@ const containerStyle = {
   
 function Maparea() {
 
+        //call useJsApiLoader with ID & googleMapsApiKey properties an save the state to isLoaded to check if loader worked
         const { isLoaded } = useJsApiLoader({
           id: 'google-map-script',
           googleMapsApiKey: "AIzaSyCekJ-T90WDgNSJrwkYvkchwSAmhq7nq8w"
         });
       
-        const [map, setMap] = React.useState(null)
+        //Define stateful 'map' value, with inital value of null
+        const [map, setMap] = React.useState(null);
       
+        //use the react useCallback to prevent the called function (callback) from recreating unless necessary
         const onLoad = React.useCallback(function callback(map) {
           // This is just an example of getting and using the map instance!!! don't just blindly copy!
           const bounds = new window.google.maps.LatLngBounds(center);
-          map.fitBounds(bounds);
+          map.fitBounds(bounds); 
       
-          setMap(map)
+          setMap(map);
         }, []);
       
+        //Define the onUnmount property as the return of callback that reset map to null via setMap
         const onUnmount = React.useCallback(function callback(map) {
           setMap(null)
         }, []);
