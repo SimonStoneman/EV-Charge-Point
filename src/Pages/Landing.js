a357ee724bb6d50f8155e16c5f57193ae56eb10d
Olasunkanmi <olasunkanmi.m.owolabi@gmail.com>
2023-02-21 19:04:15 +0000

    path to suggestBar updated

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -1,10 +1,14 @@
 
+import SuggestBar from "../Components/SuggestBar/suggestBar";
 
 function Landing() {
     return (
       <>
         <h1>Landing</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, qui veniam, quas, possimus nulla cumque corporis fugit impedit dolor quibusdam suscipit? Id doloremque mollitia aliquid, molestiae magnam quae? Libero, dicta?</p>
+
+        <SuggestBar />
+
       </>
     )
   }
