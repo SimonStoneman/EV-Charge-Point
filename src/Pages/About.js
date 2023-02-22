dcc0c1cc7473793ef50e720b078517a55b890c08
kevmcgowan <78913135+kevmcgowan@users.noreply.github.com>
2023-02-22 18:03:44 +0000

    Created an about page, but cannot get it to render yet

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,10 +1,88 @@
-function About() {
-    return (
-      <>
-        <h1>About</h1>
-        <img src="https://dummyimage.com/600x400/000/fff" alt="" />
-      </>
-    )
-  }
+import { useEffect, useState } from 'react'
+import {
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faLinkedin,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../Components/AnimatedLetters/AnimatedLetters'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import './about.scss'
+
+const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
+  return (
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'U', 's']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            We are a group of bootcamp coders new to REACT JS.
+          </p>
+          <p align="LEFT">
+            This is the final project of a 16 week Bootcamp where we were introduced to Frontend Web Development.
+          </p>
+          
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faLinkedin} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default About
+
+
+
+
+
+
+// function About() {
+//     return (
+//       <>
+//         <h1>About</h1>
+//         <img src="https://dummyimage.com/600x400/000/fff" alt="" />
+//       </>
+//     )
+//   }
   
-  export default About;
+//   export default About;
--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -10,8 +10,7 @@ function Landing() {
     return (
       <>
         <h1>Landing</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, qui veniam, quas, possimus nulla cumque corporis fugit impedit dolor quibusdam suscipit? Id doloremque mollitia aliquid, molestiae magnam quae? Libero, dicta?</p>
-
+        
 
         <Sidebar />
 
