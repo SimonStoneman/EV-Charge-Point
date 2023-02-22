3df9c98d22de8c4baf67ac843724fa8b2d16ea5b
kevmcgowan <78913135+kevmcgowan@users.noreply.github.com>
2023-02-22 20:10:10 +0000

    created the browser routes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-
-import React, { useState } from 'react';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Landing from './Pages/Landing';
 import About from './Pages/About';
-import Contact from './Pages/Contact'
-import Footer from './Components/Footer/Footer'
+import Contact from './Pages/Contact';
+import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 
 
 function App() {
-  const [page, setPage] = useState('landing');
-
   return (
     <>
-      <Header setPage={setPage} />
-      
-      {page === 'landing' ? <Landing /> : <About /> && <Contact /> }
-
-
-
-      <Footer />
-
+      <Routes>
+        <Route path="/" element={<Landing />}>
+          <Route index element={<Landing />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/footer" element={<Footer />} />
+          <Route path="/header" element={<Header />} />
+        </Route>
+      </Routes>
     </>
-  );
+  )
 }
 
 export default App;
