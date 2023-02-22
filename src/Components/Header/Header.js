8f57a7fac5e95d1ff22e7b528fd06b1af83972d8
JD Tadlock <jt7903@gmail.com>
2023-02-22 13:24:17 -0500

    landing styles started

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,49 +1,49 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faUser, faEnvelope, faBars } from '@fortawesome/free-solid-svg-icons';
+import { Nav, Navbar, Container, NavDropdown } from 'react-bootstrap';
 import risky from '../../assets/images/risky.jpeg'
 import './header.css'
 
 function Header() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <img src={risky} alt='logo' />
-        <a className="navbar-brand" href="/">
-        EV Charge Point Food & Ent Suggester
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="/">
-                <FontAwesomeIcon icon={faHome} color='#ffd700'/> Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/about">
-                <FontAwesomeIcon icon={faUser} color='#ffd700' /> About
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/contact">
-                <FontAwesomeIcon icon={faEnvelope} color='#ffd700'/> Contact
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <Navbar expand="lg">
+      <Container>
+        <Navbar.Brand href="/">
+          <img src={risky} alt='logo' />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" variant="light">
+          <FontAwesomeIcon
+            icon={faBars}
+            size="lg"
+            color='#ddd' />
+        </Navbar.Toggle>
+
+        <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+          <Nav className="d-flex justify-content-center">
+            <Nav.Item>
+              <Nav.Link href="/">
+                <FontAwesomeIcon icon={faHome} color='#ffd700' />
+                <span>Home</span>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/about">
+                <FontAwesomeIcon icon={faUser} color='#ffd700' />
+                <span>About</span>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color='#ffd700' />
+                <span>Contact</span>
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
   );
 }
 
@@ -51,4 +51,3 @@ export default Header;
 
 
 
-     
--- src/Components/Maparea/Maparea.js
+++ src/Components/Maparea/Maparea.js
@@ -4,14 +4,16 @@ import EVchargepoints from './EVchargepoints'
 import './Maparea.css'
 
 const containerStyle = {
-    width: '1000px',
-    height: '1000px'
-  };
-  
-  const center = {
-    lat: -3.745,
-    lng: -38.523
-  };
+  height: '400px',
+  width: '500px',
+  maxWidth: '100%',
+  maxHeight: '45vh'
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
 
 //Defining the libraries required from google maps on loading (useJsApiLoader). Recommended to use this, as arrays & objects used a literals look to react as a new item and can cause re-rendering
 const libraries = ["places"];
@@ -23,90 +25,93 @@ const options = {
 
 var pos;
 var bounds;
-  
+
 function Maparea() {
 
-        //call useJsApiLoader with ID & googleMapsApiKey properties an save the state to isLoaded to check if loader worked. Using process.env to load hidden API key in env.local file. Adding additional functionalities via libraries prop to add 'place'
-        const { isLoaded } = useJsApiLoader({
-          id: 'google-map-script',
-          googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-          libraries,
-        });
-      
-        //Define stateful 'map' value, with inital value of null
-        const [map, setMap] = React.useState(null);
+  //call useJsApiLoader with ID & googleMapsApiKey properties an save the state to isLoaded to check if loader worked. Using process.env to load hidden API key in env.local file. Adding additional functionalities via libraries prop to add 'place'
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
+  });
 
-        
-      
-        //use the react useCallback to prevent the called function (callback) from recreating unless necessary
-        const onLoad = React.useCallback(function callback(map) {
-          // This is just an example of getting and using the map instance!!! don't just blindly copy!
+  //Define stateful 'map' value, with inital value of null
+  const [map, setMap] = React.useState(null);
 
-          if (navigator.geolocation) {
-                //Call the geolocation API of the browser window.navigator object, using the getCurrentPosition method to obtain from the users device the current location co-ords
-                navigator.geolocation.getCurrentPosition(
-                    (position) => {
-                        pos = {
-                            lat: position.coords.latitude,
-                            lng: position.coords.longitude,
-                        };
-            
-                        bounds = new window.google.maps.LatLngBounds(pos);
-                        //call the fitBounds method of map object to the co-ords from pos
-                        map.fitBounds(bounds); 
-                        map.setZoom(14);
-                    
-                        // //Set the map state from null to true;GoogleMap
-                        // console.log(map);
-                        setMap(map);
-                    },
-                    () => {
-                        // handleLocationError(true, infoWindow, map.getCenter());
-                    }
-                );
-          }
-            // } else {
-            //     // Browser doesn't support Geolocation
-                // handleLocationError(false, infoWindow, map.getCenter());
-            // };
 
-     
 
-        }, []);
-      
-        //Define the onUnmount property as the return of callback that reset map to null via setMap
-        const onUnmount = React.useCallback(function callback(map) {
-          setMap(null)
-        }, []);
+  //use the react useCallback to prevent the called function (callback) from recreating unless necessary
+  const onLoad = React.useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
 
-        //Define the onClick action of InfoWindows
+    if (navigator.geolocation) {
+      //Call the geolocation API of the browser window.navigator object, using the getCurrentPosition method to obtain from the users device the current location co-ords
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
 
-        const [selectedinfows, setSelectedInfoWS] = React.useState(null);
+          bounds = new window.google.maps.LatLngBounds(pos);
+          //call the fitBounds method of map object to the co-ords from pos
+          map.fitBounds(bounds);
+          map.setZoom(14);
 
-        const onClickInfoW = React.useCallback(function callback(map) {
-        },[]);
+          // //Set the map state from null to true;GoogleMap
+          // console.log(map);
+          setMap(map);
+        },
+        () => {
+          // handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    }
+    // } else {
+    //     // Browser doesn't support Geolocation
+    // handleLocationError(false, infoWindow, map.getCenter());
+    // };
 
-        return isLoaded ? (
-          <>
-            <h1 className="mapHeader">Charge Mapz <span role="img" aria-label="lightning">⚡</span></h1>
 
-            <GoogleMap
-              mapContainerStyle={containerStyle}
-              center={pos}
-              zoom={10}
-              onLoad={onLoad}
-              onUnmount={onUnmount}
-              options={options}
-            >
-              { /* Child components, such as markers, info windows, etc. */ }
 
-              {/* Marker current users position */}
-              <Marker position={pos}></Marker>
-              <EVchargepoints map={map}/>
-              
-            </GoogleMap>
-          </>
-        ) : <></>
+  }, []);
+
+  //Define the onUnmount property as the return of callback that reset map to null via setMap
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, []);
+
+  //Define the onClick action of InfoWindows
+
+  const [selectedinfows, setSelectedInfoWS] = React.useState(null);
+
+  const onClickInfoW = React.useCallback(function callback(map) {
+  }, []);
+
+  return isLoaded ? (
+    <div className="landing-content">
+
+
+      <div className="map-wrapper">
+        <h1 className="mapHeader">Charge Mapz <span role="img" aria-label="lightning">⚡</span></h1>
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={pos}
+          zoom={10}
+          onLoad={onLoad}
+          onUnmount={onUnmount}
+          options={options}
+        >
+          { /* Child components, such as markers, info windows, etc. */}
+
+          {/* Marker current users position */}
+          <Marker position={pos}></Marker>
+          <EVchargepoints map={map} />
+
+        </GoogleMap>
+      </div>
+    </div>
+  ) : <></>
 };
 
 export default Maparea;
